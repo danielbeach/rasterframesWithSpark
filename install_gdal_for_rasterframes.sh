993574d81973998b398864eba2d03b97d4a8b653
Daniel B <34192225+danielbeach@users.noreply.github.com>
2020-10-12 08:11:13 -0500

    Create install_gdal_for_rasterframes.sh

---
+++ install_gdal_for_rasterframes.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3.6-dev
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+pip3 install GDAL==2.4.2
